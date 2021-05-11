9127597d8adb3dddc52d9beaad342a4a263f7230
jaywomack <womackjw@hotmail.com>
2021-05-11 07:44:50 -0700

    Finished oop series and starting on decorators

--- classes_inheritance.py
+++ classes_inheritance.py
@@ -7,17 +7,42 @@ class Employee:
     num_of_emps = 0
     raise_amt = 1.04
 
-    def __init__(self, first, last, pay, phone):
+    def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
-        self.email = first + '.' + last + '@company.com'
-        self.phone = phone
-
         Employee.num_of_emps += 1
 
+    def __repr__(self):
+        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+
+    # def __str__(self):
+    #     return '{} - {}'.format(self.fullname(), self.email)
+    #
+    # def __add__(self, other):
+    #     return self.pay + other.pay
+    #
+    # def __len__(self):
+    #     return len(self.fullname())
+
+    @property
     def fullname(self):
         return f"{self.first} {self.last}"
+
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
+
+    @fullname.deleter
+    def fullname(self):
+        print('Delete Name!')
+        self.first = None
+        self.last = None
+    @property
+    def email(self):
+        return f"{self.first}.{self.last}@email.com"
 
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
@@ -26,12 +51,64 @@ class Employee:
     def set_raise_amt(cls, amount):
         cls.raise_amt = amount
 
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
 
-emp_1 = Employee('Corey', 'Schafer', 50000, '509')
-emp_2 = Employee('Jay', 'Womack', 150000, '208')
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 
-Employee.set_raise_amt(1.05)
 
-print(Employee.raise_amt)
-print(emp_1.raise_amt)
-print(emp_2.raise_amt)
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
+emp_str_3 = 'Jane-Doe-90000'
+
+new_emp_1 = Employee.from_string(emp_str_1)
+new_emp_2 = Employee.from_string(emp_str_2)
+new_emp_3 = Employee.from_string(emp_str_3)
+
+print(new_emp_1.fullname)
+new_emp_1.fullname = 'Jared Womack'
+print(new_emp_1.fullname)
+
+class Developer(Employee):
+    raise_amt = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('__>', emp.fullname())
+
+
+dev = Developer('Jay', 'Womack', 90000, 'Python')
+
+jay = Manager('Jay', 'Womack', 120000, [dev])
+whitney = Manager('Whitney', 'Womack', 1, [])
+
+
+print(new_emp_1)
